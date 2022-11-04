0513834329ffa807dcd532732f58ac042f2c03bf
Amanda Cheung <ac119@cs.wellesley.edu>
2022-11-04 11:11:27 -0400

    added the insert statements into the sample data addition file

---
+++ sample_data/add_sample_data.sql
@@ -0,0 +1,13 @@
+--- Adding Sample Data to Database----
+
+use sibconn_db;
+
+insert into user(uid, name, email, pronouns, class_year)
+
+insert into seeking_post(pid, uid, category, description)
+
+insert into event_post(pid, uid, category, location, date_time,length, recurring, capacity, skill, description)
+
+insert into category(cid, name)
+
+insert into comment(commentid, pid, uid, commenttext)

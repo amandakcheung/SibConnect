eed3b8250a18619ac75d27ca28bf0ea6a7c20aca
Amanda Cheung <ac119@cs.wellesley.edu>
2022-11-04 11:05:15 -0400

    added director for all of p2 and created a file for the adding of sample data

--- sample_data/sample_data.sql
+++ sample_data/sample_data.sql
@@ -1,9 +1,12 @@
+----Inserting Sample Data------
+use sibconn_db;
 
-drop table if exists user;
 drop table if exists seeking_post;
 drop table if exists event_post;
 drop table if exists comment;
 drop table if exists category;
+drop table if exists user;
+
 
 
 CREATE TABLE `user` (
@@ -12,39 +15,52 @@ CREATE TABLE `user` (
   `email` varchar(60),
   `pronouns` varchar(20),
   `class_year` int(4)
-);
+)
+
+ENGINE = InnoDB;
+
 
 CREATE TABLE `seeking_post` (
   `pid` int PRIMARY KEY,
   `uid` int,
   `category` int,
   `description` text
-);
+)
+
+ENGINE = InnoDB;
 
 CREATE TABLE `event_post` (
   `pid` int PRIMARY KEY,
   `uid` int,
   `category` int,
   `location` ENUM ('tower', 'quint', 'new_dorms', 'stone_d', 'branch'),
-  `date_time` timedate,
+  `date_time` datetime,
   `length` int,
   `recurring` boolean,
   `capacity` int,
   `skill` int,
   `description` text
-);
+)
+
+ENGINE = InnoDB;
+
 
 CREATE TABLE `category` (
   `cid` int PRIMARY KEY,
   `name` varchar(30)
-);
+)
+
+ENGINE = InnoDB;
 
 CREATE TABLE `comment` (
   `commentid` int PRIMARY KEY,
   `pid` int,
   `uid` int,
   `commenttext` text
-);
+)
+
+ENGINE = InnoDB;
+
 
 ALTER TABLE `comment` ADD FOREIGN KEY (`uid`) REFERENCES `user` (`uid`);
 
